e0dd93c4cd1f298c4e99f819ae18c0200f84be2f
shaxu22 <shaxu@rambler.ru>
2024-11-16 08:53:57 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BonusMilesService x = new BonusMilesService();
+        int re = x.calculate(1000);
+        System.out.println(re);
+
+
+
+
+
+    }
+}
